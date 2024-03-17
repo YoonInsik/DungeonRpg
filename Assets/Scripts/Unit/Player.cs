ea9054722e88d00e990511a3aa04fc8f8396b43d
lsj733138 <lsj733138@gmail.com>
2024-03-17 11:15:14 +0900

    플레이어 이동 초간단 구현

--- Assets/Scripts/Unit/Player.cs
+++ Assets/Scripts/Unit/Player.cs
@@ -4,31 +4,13 @@ using UnityEngine;
 
 public class Player : BaseUnit
 {
+    public Vector3 newPos;
+    public float speed = 3.0f;
     void Update()
     {
-        if ((Input.GetKeyDown(KeyCode.LeftArrow)) ||
-            (Input.GetKeyDown(KeyCode.RightArrow)))
-        {
-            var horizontalValue = Input.GetAxisRaw("Horizontal");
-            //var verticalValue = Input.GetAxisRaw("Vertical");
+        newPos.x = Input.GetAxisRaw("Horizontal");
+        newPos.y = Input.GetAxisRaw("Vertical");
 
-            Vector3 newPos = transform.position;
-            newPos.x += horizontalValue;
-            //newPos.y += verticalValue;
-
-            transform.position = newPos;
-        }
-        else if ((Input.GetKeyDown(KeyCode.UpArrow)) ||
-                 (Input.GetKeyDown(KeyCode.DownArrow)))
-        {
-            //var horizontalValue = Input.GetAxisRaw("Horizontal");
-            var verticalValue = Input.GetAxisRaw("Vertical");
-
-            Vector3 newPos = transform.position;
-            //newPos.x += horizontalValue;
-            newPos.y += verticalValue;
-
-            transform.position = newPos;
-        }
+        transform.position += newPos.normalized * speed * Time.deltaTime;
     }
 }
