fb23503ec61415f689acb3e13e9d60014f7cbdc2
lsj733138 <lsj733138@gmail.com>
2024-03-19 16:00:39 +0900

    플레이어 이동, 고기와 충돌 추가

--- Assets/Scripts/Unit/Player.cs
+++ Assets/Scripts/Unit/Player.cs
@@ -1,16 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : BaseUnit
 {
-    public Vector3 newPos;
+    public Vector2 newPos;
+    public Rigidbody2D rigid;
     public float speed = 3.0f;
-    void Update()
+    private Inventory inventory;
+
+    private void Start()
+    {
+        inventory = Inventory.instance;
+    }
+    private void Update()
     {
         newPos.x = Input.GetAxisRaw("Horizontal");
         newPos.y = Input.GetAxisRaw("Vertical");
+    }
+    void FixedUpdate()
+    {
+        rigid.MovePosition((newPos.normalized * speed * Time.fixedDeltaTime) + rigid.position);
+    }
 
-        transform.position += newPos.normalized * speed * Time.deltaTime;
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Meat"))
+        {
+            Debug.Log("gg");
+            MeatItem meat = collision.gameObject.GetComponent<ItemImplement>().item;
+
+            if (meat != null)
+            {
+                inventory.AddMeat(meat);
+                Debug.Log(meat.name);
+            }
+
+            Destroy(collision.gameObject);
+        }
     }
 }
