9ee878d5027512207efb14ad8e8624b612f07765
YoonInsik <57671008+YoonInsik@users.noreply.github.com>
2024-04-13 23:42:27 +0900

    next chunk UI linked chunkData

---
+++ Assets/Scripts/MapGen/DropTableUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropTableUI : MonoBehaviour
+{
+    [SerializeField] private List<Image> dropItemImages;
+
+    public void SetDropItemImages(List<DropData> droptable)
+    {
+        for (int i = 0; i < droptable.Count; i++)
+        {
+            dropItemImages[i].sprite = droptable[i].item.GetComponent<ItemImplement>().item.icon;
+            dropItemImages[i].gameObject.SetActive(true);
+        }
+        for (int i = droptable.Count; i < dropItemImages.Count; i++)
+        {
+            dropItemImages[i].gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/MapGen/MapData.cs
+++ Assets/Scripts/MapGen/MapData.cs
@@ -23,6 +23,33 @@ public class ChunkData
 
     public List<DropData> dropTable;
     public ChunkType chunkType;
+
+    public string StringChunkType 
+    { 
+        get 
+        {
+            string value;
+            switch (chunkType)
+            {
+                case ChunkType.Start:
+                    value = "시작";
+                    break;
+                case ChunkType.Boss:
+                    value = "보스";
+                    break;
+                case ChunkType.Normal:
+                    value = "일반";
+                    break;
+                case ChunkType.Many:
+                    value = "더 많은 적";
+                    break;
+                default:
+                    value = "오류 방";
+                    break;
+            }
+            return value;
+        }
+    }
 }
 
 public enum ChunkType
